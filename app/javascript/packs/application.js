9a5787165ff8932ead351aa6a14ef686da5a0091
akitayuri <nmnl_y.bgux@icloud.com>
2021-05-31 16:08:58 +0900

    add コーティング作業

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,11 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//= require rails-ujs
+//= require turbolinks
+//= require jquery/dist/jquery.js
+//= require bootstrap/dist/js/bootstrap.min
+//= require_tree
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
