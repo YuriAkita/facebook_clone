78e8da6cd2f2849f6409784a02ad3e35bfc462d0
akitayuri <nmnl_y.bgux@icloud.com>
2021-05-31 13:11:12 +0900

    fix 不要なコメント削除

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,26 +1,21 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: %i[ show edit update destroy ]
 
-  # GET /blogs or /blogs.json
   def index
     @blogs = Blog.all
   end
 
-  # GET /blogs/1 or /blogs/1.json
   def show
     @favorite = current_user.favorites.find_by(blog_id: @blog.id)
   end
 
-  # GET /blogs/new
   def new
     @blog = Blog.new
   end
 
-  # GET /blogs/1/edit
   def edit
   end
 
-  # POST /blogs or /blogs.json
   def create
     @blog = current_user.blogs.build(blog_params)
     if params[:back]
@@ -38,7 +33,6 @@ class BlogsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /blogs/1 or /blogs/1.json
   def update
     respond_to do |format|
       if @blog.update(blog_params)
@@ -51,7 +45,6 @@ class BlogsController < ApplicationController
     end
   end
 
-  # DELETE /blogs/1 or /blogs/1.json
   def destroy
     @blog.destroy
     respond_to do |format|
@@ -66,12 +59,10 @@ class BlogsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_blog
       @blog = Blog.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def blog_params
       params.require(:blog).permit(:image, :image_cache, :content, :user_id)
     end
