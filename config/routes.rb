3c50ac0c8af0643e477b6ab6b8f547b7c339bf64
akitayuri <nmnl_y.bgux@icloud.com>
2021-07-04 22:52:30 +0900

    create conversations controller, add action +index +create

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,17 @@ Rails.application.routes.draw do
   get 'relationships/create'
   get 'relationships/destroy'
   get '/',to: "blogs#index"
+
   resources :blogs do
     collection do
       post :confirm
     end
   end
+
+  resources :conversations do
+    resources :messages
+  end
+
   resources :users, only: [:index, :new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
   resources :favorites, only: [:create, :destroy]
